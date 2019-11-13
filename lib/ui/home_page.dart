1b48b15f179d1f2d2b50dce1f4e4b543a3a4ff9f
Henrique La Montagna <henriquelamontagna@gmail.com>
2019-11-13 16:30:32 -0300

    Criando o Projeto e Fazendo a Requisição de GIFs na API

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,40 @@
+import 'dart:async' as prefix0;
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _search;
+  int _offset;
+
+  Future<Map> _getGifs() async {
+    http.Response response;
+    if (_search == null)
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=9VIEFrc21Wlm0BHl30FIcCGqiDFovLq2&limit=20&rating=G");
+    else
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=9VIEFrc21Wlm0BHl30FIcCGqiDFovLq2&q=$_search&limit=25&offset=$_offset&rating=G&lang=pt");
+    return json.decode(response.body);
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    _getGifs().then((map){
+      print(map);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
