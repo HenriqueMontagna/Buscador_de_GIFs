8b1c74e7d9012d3147caf8a978e390ada7b80c12
Henrique La Montagna <henriquelamontagna@gmail.com>
2019-11-19 12:10:49 -0300

    Compartilhamento Add; Gif Fade Add; Gif Page Add - App Pronto.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:buscador_gifs/ui/gif_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:buscador_gifs/ui/home_page.dart';
+
 
 void main() => runApp(MaterialApp(
   theme: ThemeData(hintColor: Colors.white),
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -2,7 +2,11 @@ import 'dart:async' as prefix0;
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
 import 'dart:async';
+import 'package:transparent_image/transparent_image.dart';
+
+import 'gif_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -95,7 +99,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   int _getCount(List data) {
-    if (_search == null) {
+    if (_search == null || _search.isEmpty) {
       return data.length;
     } else {
       return data.length + 1;
@@ -110,11 +114,24 @@ class _HomePageState extends State<HomePage> {
         itemBuilder: (context, index) {
           if (_search == null || index < snapshot.data["data"].length) {
             return GestureDetector(
-              child: Image.network(
-                snapshot.data["data"][index]["images"]["fixed_height"]["url"],
-                height: 300.0,
-                fit: BoxFit.cover,
-              ),
+              child: FadeInImage.memoryNetwork(
+                  placeholder: kTransparentImage,
+                  image: snapshot.data["data"][index]["images"]["fixed_height"]
+                      ["url"],
+                  height: 300.0,
+                  fit: BoxFit.cover),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          GifPage(snapshot.data["data"][index])),
+                );
+              },
+              onLongPress: () {
+                Share.share(snapshot.data["data"][index]["images"]
+                    ["fixed_height"]["url"]);
+              },
             );
           } else {
             return Container(
